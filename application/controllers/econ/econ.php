6db9dee63fad795669b8de80772142f95ad8a0c9
Alex Cazacu <alexandru.cazacu92@gmail.com>
2012-10-28 22:26:01 +0200

    rmv

--- application/controllers/econ/econ.php
+++ application/controllers/econ/econ.php
@@ -12,17 +12,17 @@ class Econ extends CI_Controller {
 	
 	public function index () {
 		
-		$this->load->model ("EconModel");				
-		$data['econlevels'] = $this->EconModel->get_econlevels ();
-		$data['econforcasts'] = $this->EconModel->get_econforcasts ();		
+		$this->load->model ("econ_model");				
+		$data['econlevels'] = $this->Econ_model->get_econlevels ();
+		$data['econforcasts'] = $this->Econ_model->get_econforcasts ();		
 		
 		$this->EconModel->apply_filter_name ($data['econforcasts'], $this->input->get_post('filter_name'));
 		
 		$filter_date['start'] = $this->input->get_post ("filter_date_start"); 
 		$filter_date['end'] = $this->input->get_post ("filter_date_end");
-		$this->EconModel->apply_filter_date ($data['econforcasts'], $filter_date);
+		$this->Econ_model->apply_filter_date ($data['econforcasts'], $filter_date);
 				
-		$this->EconModel->apply_filter_type ($data['econforcasts'], $this->input->get_post('filter_type'));
+		$this->Econ_model->apply_filter_type ($data['econforcasts'], $this->input->get_post('filter_type'));
 
 		$this->load->view ("econ", $data); 
    }
