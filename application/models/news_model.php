00afe92593e2476234c7ab1020da93795bc54879
root <root@vps.creativeprogramming.co.uk>
2012-10-28 20:05:26 +0000

    renamed models to codeigniter naming convention

---
+++ application/models/news_model.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	class News_model extends CI_Model {
+		
+		public function __construct() {
+			
+                        parent::__construct();
+		}
+	
+		public function get_news() {
+		
+			$this->db->select("*");
+			$this->db->from("news");
+			$this->db->join("countries"," countries.id = countries_id ","left") ;
+			$query = $this->db->get() ; 
+			return $query->result_array();
+		}
+	}
+	
+?>	
